9cfaa07a537136c9704c8a5daed44464515a7a21
Harshal-Mahajan-Devep <mahajanharshalc171106@gmail.com>
2026-01-14 11:32:59 +0530

    work on intermanage

--- src/Component/InterManage.js
+++ src/Component/InterManage.js
@@ -1,11 +1,4 @@
 import React, { useEffect, useRef, useState } from "react";
-import axios from "axios";
-import Pagination from "./commenuse/Pagination";
-import ConfirmDelete from "./commenuse/ConfirmDelete";
-import { toast, ToastContainer } from "react-toastify";
-import { useForm } from "react-hook-form";
-import { BASE_URL } from "../config/constants";
-import TableSkeleton from "./commenuse/TableSkeleton";
 
 function InterManage() {
   return <>INTERVIEW MANAGEMENT</>;
